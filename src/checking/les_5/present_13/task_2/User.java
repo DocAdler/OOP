be66f3794201c5645af007f33995003d5ff0a4e6
DocAdler <dr.adler.orel@gmail.com>
2022-06-14 15:31:19 +0300

    For checking Les_5

---
+++ src/checking/les_5/present_13/task_2/User.java
@@ -0,0 +1,65 @@
+package checking.les_5.present_13.task_2;
+
+import org.jetbrains.annotations.NotNull;
+
+public class User implements Comparable<User>{
+
+    String name;
+    String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    @Override
+    public int compareTo(@NotNull User user) {
+        int result = this.name.compareTo(user.name);
+        if (result == 0) {
+            result = this.email.compareTo(user.email);
+        }
+        return result;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (!name.equals(user.name)) return false;
+        return email.equals(user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + email.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
---
+++ src/checking/les_5/present_13/task_3/UsersComparator.java
@@ -0,0 +1,11 @@
+package checking.les_5.present_13.task_3;
+
+import java.util.Comparator;
+
+public class UsersComparator implements Comparator<Users> {
+
+    @Override
+    public int compare(Users users1, Users users2) {
+        return users1.email.length() - users2.email.length();
+    }
+}
