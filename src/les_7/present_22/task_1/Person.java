df307b51f818c24d763a37c15d97b421a974e37c
DocAdler <dr.adler.orel@gmail.com>
2022-06-21 18:27:21 +0300

    Home Work Les_7

---
+++ src/les_7/present_22/task_1/Person.java
@@ -0,0 +1,58 @@
+package les_7.present_22.task_1;
+
+
+import java.util.Calendar;
+
+public class Person {
+
+    private String name;
+    private Integer age;
+    private Calendar calendar;
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Calendar getCalendar() {
+        return calendar;
+    }
+
+    public void setCalendar(Calendar calendar) {
+        this.calendar = calendar;
+    }
+
+    @Override
+    public String toString() {
+        if (calendar == null) {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", age=" + age +
+                    '}';
+
+        } else {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", age=" + age +
+                    ", calendar=" + calendar.getTime() +
+                    '}';
+        }
+
+    }
+}
---
+++ src/les_7/present_22/task_3/MyLambda.java
@@ -0,0 +1,8 @@
+package les_7.present_22.task_3;
+
+@FunctionalInterface
+public interface MyLambda {
+
+    int test (Integer value1, Integer value2, Integer value3);
+
+}
