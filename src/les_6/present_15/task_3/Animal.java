eaf16f0f3f97e9cef543937be27202072c234ce9
DocAdler <dr.adler.orel@gmail.com>
2022-06-21 18:26:03 +0300

    Home Work Les_6

---
+++ src/les_6/present_15/task_3/Animal.java
@@ -0,0 +1,51 @@
+package les_6.present_15.task_3;
+
+public class Animal {
+
+    private String type;
+    private String name;
+
+    public Animal(String type, String name) {
+        this.type = type;
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Animal animal = (Animal) o;
+
+        if (!type.equals(animal.type)) return false;
+        return name.equals(animal.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = type.hashCode();
+        result = 31 * result + name.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "    Питомец: " + type + " по имени " + name + ";";
+    }
+}
---
+++ src/les_6/present_15/task_3/ZooClubInter.java
@@ -0,0 +1,14 @@
+package les_6.present_15.task_3;
+
+public interface ZooClubInter {
+
+    void mainMenu();
+    void addPerson();
+    Person makePerson();
+    void addAnimal();
+    Animal makeAnimal();
+    void removePerson();
+    void removeAnimal();
+    void showClub();
+    void end();
+}
